4ab18c7508933c85b54a1ba827b481f6b1af60af
lianzhao <knife.fe@gmail.com>
2015-08-05 00:44:57 +0800

    init commit

---
+++ userscript.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Huiji.AllWikis
+// @namespace    Huiji
+// @version      0.0.1
+// @description  显示所有灰机维基
+// @author       lianzhao
+// @match        http://*.huiji.wiki/*
+// @grant        none
+// ==/UserScript==
+
+$(function(){
+    var current = window.location.href;
+	var index = current.indexOf('.huiji.wiki');
+	$('.dropdown-menu').append('<li><a href="/wiki/特殊:跨wiki">所有Wiki</a></li>');
+	
+	if (current.indexOf('.huiji.wiki/wiki/%E7%89%B9%E6%AE%8A:%E8%B7%A8wiki') > 0){
+        $('.mw-interwikitable-row').filter(function(i, e){
+            return $(e).children('.mw-interwikitable-local-yes').length > 0
+        }).each(function(i, e){
+            var cell = $(e).children()[0];
+            var text = $(cell).text();
+            $(cell).html('<a href="http://' + text + '.huiji.wiki/">' + text + '</a>');
+        })
+	}
+});
